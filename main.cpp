bfb65854f0f834d9c6224574fabc1c731a9c8bf2
MacBook <MacBook@192.168.0.25>
2017-06-19 02:56:42 -0500

    solucion Ingenua lista

--- main.cpp
+++ main.cpp
@@ -22,11 +22,17 @@ using namespace std;
 void cargaArchivo(string dir);
 void solIngenua();
 
+
+struct division{
+    int inicio;
+    int final;
+};
+
 int numLibros;
 int numEscritores;
 vector<string> libros;
 vector<int> paginas;
-vector<vector> posiblesSol;
+vector<vector<division> > posiblesSol;
 /*
  * 
  */
@@ -39,35 +45,35 @@ int main(int argc, char** argv) {
     cargaArchivo(direccion);
     
     
-//    bool opc =true;
-//    
-//    while(opc){
-//        
-//        int selec;
-//        cout << endl << "Ingrese 1 para la solución INGENUA "<< endl << "Ingrese 3 para la solución DINAMICA "<< endl << "Ingrese 2 para la solución VORAZ "<< endl ; 
-//        cin >> selec;
-//
-//        switch(selec){
-//            case 1:
-//                cout << "Aqui ira la solucion ingenua";
-//                solIngenua();
-//                opc = false;
-//                break;
-//            case 2:
-//                cout << "Aqui ira la solucion dinamica";
-//                opc = false;
-//                break;
-//            case 3:
-//                cout << "Aqui ira la solucion voraz";
-//                opc = false;
-//                break;
-//            default:
-//                cout << "Ingrese una opcion válida" ;
-//                break;
-//        }
-//    
-//    }
-//    //
+    bool opc =true;
+    
+    while(opc){
+        
+        int selec;
+        cout << endl << "Ingrese 1 para la solución INGENUA "<< endl << "Ingrese 3 para la solución DINAMICA "<< endl << "Ingrese 2 para la solución VORAZ "<< endl ; 
+        cin >> selec;
+
+        switch(selec){
+            case 1:
+                cout << "La solución optima calculada de forma exhaustiva es la siguiente:"<<endl;
+                solIngenua();
+                opc = false;
+                break;
+            case 2:
+                cout << "Aqui ira la solucion dinamica";
+                opc = false;
+                break;
+            case 3:
+                cout << "Aqui ira la solucion voraz";
+                opc = false;
+                break;
+            default:
+                cout << "Ingrese una opcion válida" ;
+                break;
+        }
+    
+    }
+    //
     
     return 0;
 }
@@ -86,11 +92,85 @@ void split(const string& s, char delim,vector<string>& v) {
     }
 }
 
-void solIngenua(){
-    
-    
+
+
+void extraerPosiblesSol(int escritor,int libros,vector<division> librosSel){
+
+	if (!(escritor==0 and libros!=0) and !(escritor!=0 and libros ==0)){
+
+		if(escritor==0 and libros==0){
+			posiblesSol.push_back(librosSel);
+
+		}else{
+
+			for(int i=1;i<=libros;i++){
+				vector<division>  auxSol = librosSel;
+				division selec;
+                                selec.inicio = i;
+                                selec.final = libros;
+				auxSol.push_back(selec);
+				extraerPosiblesSol(escritor-1,i-1,auxSol);
+
+			}
+		}
+	}
+}    
     
 
+
+int calcTiempo(division div){
+    
+    int tiempo =0;
+    for(int i = div.inicio; i<=div.final ;i++){
+        tiempo = tiempo + paginas[i-1];
+    }
+    
+    return tiempo;
+}
+    
+int calcularTiempo(vector<division> sol){
+    int tiempo = 0;
+    
+    for(int i = 0; i < sol.size();i++){
+        if(calcTiempo(sol[i]) > tiempo){
+            tiempo = calcTiempo(sol[i]);
+        }
+    }
+    
+    return tiempo;
+}
+    
+
+
+void solIngenua(){
+    
+    extraerPosiblesSol(numEscritores,numLibros,vector<division>());
+    
+    vector<division> solucion;
+    int tiempoMin =0;
+    
+    for(int i = 0; i<posiblesSol.size();i++){
+        
+        if(i==0){
+           tiempoMin = calcularTiempo(posiblesSol[i]);
+           solucion = posiblesSol[i];
+        }else if(tiempoMin > calcularTiempo(posiblesSol[i])){
+            tiempoMin = calcularTiempo(posiblesSol[i]);
+            solucion = posiblesSol[i];
+        }
+    }
+    cout<<"El tiempo total sera de "<<tiempoMin<<" dias."<<endl<<"Y se distribuira el trabajo de la siguiente forma:"<<endl;
+    
+    for(int j = 0;j<solucion.size();j++){
+        if(solucion[j].inicio == solucion[j].final){
+            cout << "Al escritor número "<< j+1 << " se le asignara el siguiente libro:"<<endl<<"Título: " << libros[solucion[j].inicio - 1]<<endl;
+        }else{
+            cout << "Al escritor número "<< j+1 << " se le asignaran los siguientes libros:"<<endl;
+            for(int k = solucion[j].inicio;k<=solucion[j].final ;k++){
+                cout << "Titulo: " << libros[k-1]<<endl;
+            }       
+        }
+    }  
 }
 
 void cargaArchivo(string dir){
@@ -127,7 +207,6 @@ void cargaArchivo(string dir){
         cout <<"Titulo: " <<libros[cont]<<" Numero de paginas: " << paginas[cont] <<endl;
         cont++;
     }
-
     archivo.close();
     
 }
